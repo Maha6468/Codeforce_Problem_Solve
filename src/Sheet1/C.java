4fffe3f39570e784c789c805a2300f85c7948b29
Maha6468 <motherma7633@gmail.com>
2025-01-24 19:44:35 +0600

    codeforce

---
+++ src/Sheet1/C.java
@@ -0,0 +1,15 @@
+package Sheet1;
+
+import java.util.Scanner;
+
+public class C {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    long X = input.nextLong();
+    long Y = input.nextLong();
+    System.out.println(X + " + " + Y + " = " + (X + Y));
+    System.out.println(X + " * " + Y + " = " + (X * Y));
+    System.out.println(X + " - " + Y + " = " + (X - Y));
+
+    }
+}
