43a0b8314e0df9d241c5fb7d5a95052ce1933600
Maha6468 <motherma7633@gmail.com>
2025-01-24 19:36:45 +0600

    codeforce

---
+++ src/Sheet2/B.java
@@ -0,0 +1,14 @@
+package Sheet2;
+
+import java.util.Scanner;
+
+public class B { public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int i = input.nextInt();
+    long l = input.nextLong();
+    char ch = input.next().charAt(0);
+    float f = input.nextFloat();
+    double d = input.nextDouble();
+    System.out.println(i + "\n" + l + "\n" + ch + "\n" + f + "\n" + d + "\n");
+    }
+}
