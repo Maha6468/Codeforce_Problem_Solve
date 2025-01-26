e4b461f39d882e0c7d43a788e2cd86f0bcfe90a8
Maha6468 <motherma7633@gmail.com>
2025-01-26 20:44:16 +0600

    codeforce

---
+++ src/Sheet1/E.java
@@ -0,0 +1,12 @@
+package Sheet1;
+
+import java.util.Scanner;
+
+public class E {
+    public static void main(String[] args) {
+    Scanner input=new Scanner (System.in);
+    double R= input.nextDouble();
+    System.out.printf("%.9f\n",(3.141592653*R*R));
+
+}
+}
