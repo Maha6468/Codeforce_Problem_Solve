0e3bfec8b7fa11baded2681c3a9c92cd7d4bac16
Maha6468 <motherma7633@gmail.com>
2025-01-29 01:31:21 +0600

    ..

---
+++ src/From800To900/Elephant.java
@@ -0,0 +1,17 @@
+package From800To900;
+
+import java.util.Scanner;
+
+public class Elephant {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        int n= input.nextInt();
+        int move=0;
+        move=n/5;
+        if(n%5!=0){
+            move++;
+        }
+        System.out.println(move);
+
+    }
+}
