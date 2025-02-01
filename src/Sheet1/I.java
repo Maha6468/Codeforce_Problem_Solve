6ac5d20e5f10f468450a154642a6ab6f4e21ab29
Maha6468 <motherma7633@gmail.com>
2025-02-01 22:43:59 +0600

    ..

---
+++ src/Sheet1/I.java
@@ -0,0 +1,17 @@
+package Sheet1;
+
+import java.util.Scanner;
+
+public class I {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int A = input.nextInt();
+            int B = input.nextInt();
+            if (A >= B) {
+                System.out.println("Yes");
+            } else
+                System.out.println("No");
+
+        }
+    }
+
